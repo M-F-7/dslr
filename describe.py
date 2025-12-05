0cb7a3953b326dd954afffc2555def7fcbc47c6c
Matteo Fernandes <mfernand@f1br10s7.clusters.42paris.fr>
2025-12-05 04:22:58 +0100

    need to get a better render for the first part, take good dataset thats better :)

--- describe.py
+++ describe.py
@@ -4,14 +4,14 @@ import math
 ex = ["Count", "Mean", "Std", "Min", "25%", "50%", "75%", "Max"]
 idx = 0
 
-db = pd.read_csv("./data.csv")
+db = pd.read_csv("./datasets/dataset_train.csv")
 
 flag = True
 
 def printEx():
     global idx
     if idx < len(ex):
-        print(ex[idx], end="      ")  
+        print(f"{ex[idx]:<8}", end="      ")  
     idx += 1
 
 def count(x):
@@ -19,16 +19,18 @@ def count(x):
     return res
 
 def mean(x):
-    res = sum(db[x]) / len(db[x])
+    values = [float(v) for v in db[x] if pd.notna(v)]
+    res = sum(values) / len(values)
     return res
 
 def std(x):
-    res = math.sqrt(sum(pow(v - mean(x), 2) for v in db[x]))
-    return res / len(db) - 1
+    values = [float(v) for v in db[x] if pd.notna(v)]
+    res = math.sqrt(sum((v - mean(x))**2 for v in values) / (len(values) - 1))
+    return res
 
 def min(x):
     try:
-        res = float(db[x].name)
+        res = float(db[x].iloc[0])
     except ValueError:
         print("Min: Error")
         exit(1)
@@ -41,7 +43,7 @@ def min(x):
 
 def max(x):
     try:
-        res = float(db[x].name)
+        res = float(db[x].iloc[0])
     except ValueError:
         print("Max: Error")
         exit(1)
@@ -58,7 +60,6 @@ def median(tab):
     else:
         sorted_feature = tab.sort_values().values
     size = len(sorted_feature)
-    tab = []
     if size % 2 == 0:
         res = (sorted_feature[(size // 2 - 1)] + sorted_feature[size // 2]) / 2 # // for int and / for float
     else:
@@ -91,42 +92,41 @@ def chooseMetric(name, metric):
         flag = False
     match metric:
         case 0:
-            print(f"{count(name):.6f} ", end="   ")
+            print(f"{count(name):>13.6f} ", end="   ")
         case 1:
-            print(f"{mean(name):.6f}", end="    ")
+            print(f"{mean(name):>13.6f}", end="    ")
         case 2:
-            print(f"{std(name):.6f}", end="    ")
+            print(f"{std(name):>13.6f}", end="    ")
         case 3:
-            print(f"{min(name):.6f}", end="    ")
+            print(f"{min(name):>13.6f}", end="    ")
         case 4:
-            print(f"{twentyFive(name):.6f}", end="    ")
+            print(f"{twentyFive(name):>13.6f}", end="    ")
         case 5:
-            print(f"{fifty(name):.6f}", end="    ")
+            print(f"{fifty(name):>13.6f}", end="    ")
         case 6:
-            print(f"{seventyFive(name):.6f}", end="    ")
+            print(f"{seventyFive(name):>13.6f}", end="    ")
         case 7:
-            print(f"{max(name)}", end="    ")
+            print(f"{max(name):>13.6f}", end="    ")
         case _: # same as default in c
             print("Unkown", end="    ")
-
-
 
 def main():
     metric = 0
     global flag
+    for feature in db:
+        if pd.api.types.is_numeric_dtype(db[feature]):
+            print(f"{feature:<11}", end = "    ")
+    print()
     while metric <= len(ex):
         for name in db:
-            try:
-                nb = float(name)
+            if pd.api.types.is_numeric_dtype(db[name]):
                 chooseMetric(name, metric)
-                if metric == 7 and idx == 8 and  name == db.iloc[:, -1].name:
-                    exit()
-                if name == db.iloc[:, -1].name:
-                    flag = True
-                    metric += 1
-                    print()
-            except ValueError:
-                pass
+            if metric == 7 and idx == 8 and  name == db.columns[-1]:
+                exit()
+            if name == db.columns[-1]:
+                flag = True
+                metric += 1
+                print()
 
 
 if (__name__ == "__main__"):
