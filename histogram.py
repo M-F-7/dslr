2ba121e1ddb9f36e78796f2d260a9603fc7124f1
Matteo Fernandes <mfernand@f1br8s14.clusters.42paris.fr>
2025-12-05 22:35:29 +0100

    histogram prob done

--- histogram.py
+++ histogram.py
@@ -30,35 +30,50 @@ def std(filterColumns):
 def calculateStd(name):
     global nbCourse
     nbCourse += 1
-    res = 0
-    for house in houses.values():
-        res += std(house[name])
-    res /= 4
-    bookMarks[name] = (res)
+    bookMarks[name] = {}
+    for house, values in houses.items():
+        bookMarks[name][house] = std(values[name])
 
-def min_dict():
-    items = list(bookMarks.items())
-    key, value = items[0]
 
-    for k, v in items:
-        if v < value:
-            key = k
-            value = v
+def findBestCourse():
+    best_course = None
+    best_diff = float("inf")
 
-    return (key, value)
+    for course, house_dict in bookMarks.items():
+        std_values = list(house_dict.values())
+        diff = max(std_values) - min(std_values)
+
+        if diff < best_diff:
+            best_diff = diff
+            best_course = course
+
+    best_house_stats = bookMarks[best_course]
+    tab = [(house, std_value) for house, std_value in best_house_stats.items()]
+
+    return best_course, tab
 
 
 def main():
     for name in db:
         if pd.api.types.is_numeric_dtype(db[name]) and name != "Index":
             calculateStd(name)
-    print(min_dict())
-    plt.legend("test")
-    plt.xlabel(bookMarks.keys)
-    plt.ylabel(bookMarks.values)
-    plt.show()
 
-    #need to set up the histogram
+    tab = findBestCourse()
+    team = [x[0] for x in tab[1]]
+    values = [x[1] for x in tab[1]]
+
+    colors = ['cyan', 'purple', 'yellow', 'red']
+
+    for i, v in enumerate(values):
+        plt.text(i, v, f"{v:.4f}", ha='center', va='bottom')
+
+    plt.ylim(min(values) * 0.95, max(values) * 1.15)
+    plt.bar(team, values, color=colors)
+    plt.xlabel("Houses")
+    plt.ylabel("Standard deviation")
+    plt.title("Standard deviation for each houses")
+
+    plt.show()
 
 if (__name__ == "__main__"):
     main()
