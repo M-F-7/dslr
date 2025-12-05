c1f0b7453f890fbd142a8ee5139fe9d0299b380a
Matteo Fernandes <mfernand@f1br10s7.clusters.42paris.fr>
2025-12-05 05:25:16 +0100

    need to set up the hostogram

--- histogram.py
+++ histogram.py
@@ -1,31 +1,64 @@
 import pandas as pd
-from describe import db, std, min
+import math
+from describe import db
+import  matplotlib.pyplot as plt
+
 
 bookMarks = {}
 nbCourse = 0
+
+
+
+houses = {
+    "Ravenclaw": db[db["Hogwarts House"] == "Ravenclaw"],
+    "Gryffindor": db[db["Hogwarts House"] == "Gryffindor"],
+    "Hufflepuff": db[db["Hogwarts House"] == "Hufflepuff"],
+    "Slytherin": db[db["Hogwarts House"] == "Slytherin"],
+}
+
+def mean_series(series):
+    values = [float(v) for v in series if pd.notna(v)]
+    return sum(values) / len(values)
+
+
+def std(filterColumns):
+    values = [float(v) for v in filterColumns if pd.notna(v)]
+    res = math.sqrt(sum((v - mean_series(filterColumns))**2 for v in values) / (len(values) - 1))
+    return res
+
 
 def calculateStd(name):
     global nbCourse
     nbCourse += 1
     res = 0
-    raven = db[db["Hogwarts House"] == "Ravenclaw"]
-    griffin = db[db["Hogwarts House"] == "Gryffindor"]
-    huffle = db[db["Hogwarts House"] == "Hufflepuff"]
-    slyther = db[db["Hogwarts House"] == "Slytherin"]
-    res += std(raven[name])
-    res += std(griffin[name])
-    res += std[huffle[name]]
-    res += std(slyther[name])
-    res / 4
+    for house in houses.values():
+        res += std(house[name])
+    res /= 4
     bookMarks[name] = (res)
+
+def min_dict():
+    items = list(bookMarks.items())
+    key, value = items[0]
+
+    for k, v in items:
+        if v < value:
+            key = k
+            value = v
+
+    return (key, value)
+
 
 def main():
     for name in db:
         if pd.api.types.is_numeric_dtype(db[name]) and name != "Index":
             calculateStd(name)
-    print(bookMarks)
+    print(min_dict())
+    plt.legend("test")
+    plt.xlabel(bookMarks.keys)
+    plt.ylabel(bookMarks.values)
+    plt.show()
 
-
+    #need to set up the histogram
 
 if (__name__ == "__main__"):
     main()
