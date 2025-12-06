3c1f5c51e75a5f0daadc9a09bae30979c6567858
Matteo Fernandes <mfernand@f1br5s3.clusters.42paris.fr>
2025-12-06 16:22:01 +0100

    wip

---
+++ pair_plot.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import seaborn as sns # type: ignore
+import matplotlib.pyplot as plt
+from describe import db
+
+def main():
+    numeric_cols = [col for col in db.columns 
+                    if pd.api.types.is_numeric_dtype(db[col]) and col != "Index"]
+
+    data = db[numeric_cols + ["Hogwarts House"]].dropna()
+
+    sns.pairplot(data, hue="Hogwarts House", diag_kind="hist", plot_kws={"alpha": 0.6})
+    plt.show()
+
+if __name__ == "__main__":
+    main()
